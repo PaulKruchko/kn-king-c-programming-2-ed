b9e32ec47392163d4dfc4170da22fd952c39685e
Paul Kruchko <pmkruchko@gmail.com>
2026-08-20 08:29:02 -0400

    Added dweight2.c

---
+++ ch02_c_fundamentals/example02_03_dimensional_weight_revisited/src/dweight2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Computes the dimensional weight of a box from user input */
+
+int main(void)
+{
+    int height, length, weight, volume, weight;
+
+    printf("Enter height of a box: ");
+    scanf("%d", &height);
+    
+    return 0;
+}
